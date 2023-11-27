a71eff2e558427e85e5ec96a264698ec0e817739
MarcelPonciano <152210878+MarcelPonciano@users.noreply.github.com>
2023-11-27 18:09:59 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/LoginController.java

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -1,10 +1,10 @@
 package com.scalesec.vulnado;
 
-import org.springframework.boot.*;
+// import org.springframework.boot.*; // Removido por GFT AI Impact Bot
+// import org.springframework.stereotype.*; // Removido por GFT AI Impact Bot
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
 import org.springframework.beans.factory.annotation.*;
 import java.io.Serializable;
 
@@ -14,11 +14,11 @@ public class LoginController {
   @Value("${app.secret}")
   private String secret;
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+  // @CrossOrigin(origins = "*") // Removido por GFT AI Impact Bot
+  @PostMapping(value = "/login", produces = "application/json", consumes = "application/json") // Alterado por GFT AI Impact Bot
   LoginResponse login(@RequestBody LoginRequest input) {
-    User user = User.fetch(input.username);
-    if (Postgres.md5(input.password).equals(user.hashedPassword)) {
+    User user = User.fetch(input.getUsername()); // Alterado por GFT AI Impact Bot
+    if (Postgres.md5(input.getPassword()).equals(user.getHashedPassword())) { // Alterado por GFT AI Impact Bot
       return new LoginResponse(user.token(secret));
     } else {
       throw new Unauthorized("Access Denied");
@@ -27,13 +27,19 @@ public class LoginController {
 }
 
 class LoginRequest implements Serializable {
-  public String username;
-  public String password;
+  private String username; // Alterado por GFT AI Impact Bot
+  private String password; // Alterado por GFT AI Impact Bot
+
+  public String getUsername() { return username; } // Incluido por GFT AI Impact Bot
+  public String getPassword() { return password; } // Incluido por GFT AI Impact Bot
 }
 
 class LoginResponse implements Serializable {
-  public String token;
+  private String token; // Alterado por GFT AI Impact Bot
+
   public LoginResponse(String msg) { this.token = msg; }
+
+  public String getToken() { return token; } // Incluido por GFT AI Impact Bot
 }
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
