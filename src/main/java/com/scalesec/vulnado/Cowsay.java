c98c190b817de95d3848ec5c4961a752fc51dc9a
MarcelPonciano <152210878+MarcelPonciano@users.noreply.github.com>
2023-12-18 11:41:23 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,26 +2,42 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Cowsay {
+
+  private static final Logger logger = LogManager.getLogger(Cowsay.class);
+  
+  private Cowsay() {
+    // private constructor to hide public one
+  }
+
   public static String run(String input) {
+    // sanitize input
+    input = input.replaceAll("[^a-zA-Z0-9 ]", "");
+    
     ProcessBuilder processBuilder = new ProcessBuilder();
     String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
+
+    // log command instead of printing
+    logger.info(cmd); 
+
     processBuilder.command("bash", "-c", cmd);
 
     StringBuilder output = new StringBuilder();
 
     try {
       Process process = processBuilder.start();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      BufferedReader reader = 
+          new BufferedReader(new InputStreamReader(process.getInputStream()));
 
       String line;
       while ((line = reader.readLine()) != null) {
         output.append(line + "\n");
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      logger.error("Error running cowsay", e);
     }
     return output.toString();
   }
