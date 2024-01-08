fd8f9d1619dfa4e418db777ebd1e1e79ef2e444c
MarcelPonciano <152210878+MarcelPonciano@users.noreply.github.com>
2024-01-08 11:39:23 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/LinksController.java

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -1,23 +1,50 @@
-package com.scalesec.vulnado;
-
-import org.springframework.boot.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.boot.autoconfigure.*;
-import java.util.List;
-import java.io.Serializable;
-import java.io.IOException;
-
-
-@RestController
-@EnableAutoConfiguration
-public class LinksController {
-  @RequestMapping(value = "/links", produces = "application/json")
-  List<String> links(@RequestParam String url) throws IOException{
-    return LinkLister.getLinks(url);
-  }
-  @RequestMapping(value = "/links-v2", produces = "application/json")
-  List<String> linksV2(@RequestParam String url) throws BadRequest{
-    return LinkLister.getLinksV2(url);
+{
+  "predictions": [
+    {
+      "groundingMetadata": [
+        {}
+      ],
+      "safetyAttributes": [
+        {
+          "categories": [],
+          "blocked": false,
+          "scores": []
+        }
+      ],
+      "citationMetadata": [
+        {
+          "citations": [
+            {
+              "endIndex": 501,
+              "startIndex": 99
+            },
+            {
+              "endIndex": 501,
+              "url": "https://github.com/meerasubbarao/vulnapp",
+              "startIndex": 99,
+              "license": "Apache-2.0"
+            }
+          ]
+        }
+      ],
+      "candidates": [
+        {
+          "author": "1",
+          "content": "package com.scalesec.vulnado;\n\nimport java.util.List;\nimport java.io.IOException;\n\n@RestController\npublic class LinksController {\n\n    @RequestMapping(value = \"/links\", produces = \"application/json\")\n    List\u003cString\u003e links(@RequestParam String url) throws IOException {\n        return LinkLister.getLinks(url);\n    }\n\n    @RequestMapping(value = \"/links-v2\", produces = \"application/json\")\n    List\u003cString\u003e linksV2(@RequestParam String url) throws BadRequest {\n        return LinkLister.getLinksV2(url);\n    }\n}"
+        }
+      ]
+    }
+  ],
+  "metadata": {
+    "tokenMetadata": {
+      "outputTokenCount": {
+        "totalTokens": 149,
+        "totalBillableCharacters": 424
+      },
+      "inputTokenCount": {
+        "totalTokens": 526,
+        "totalBillableCharacters": 1828
+      }
+    }
   }
 }
