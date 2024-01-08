40e00502b701abc01b1b928cda72d4efc9031ef3
MarcelPonciano <152210878+MarcelPonciano@users.noreply.github.com>
2024-01-08 11:48:05 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -1,15 +1,1 @@
-package com.scalesec.vulnado;
-
-import org.springframework.web.bind.annotation.*;
-import org.springframework.boot.autoconfigure.*;
-
-import java.io.Serializable;
-
-@RestController
-@EnableAutoConfiguration
-public class CowController {
-    @RequestMapping(value = "/cowsay")
-    String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
-        return Cowsay.run(input);
-    }
-}
+The security token included in the request is invalid. Error Code: UnrecognizedClientException
